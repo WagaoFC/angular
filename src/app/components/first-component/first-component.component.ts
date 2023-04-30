f3732312dc3835b316974a470258f3f4a3604cde
WagaoFC <wagnerferreira_07@hotmail.com>
2023-04-30 14:37:34 -0300

    :sparkles: feat: criando interpolacao de dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+  name: string = 'WagaoFC'
+  age: number = 28
+  job: string = 'Developer'
+
+  constructor(){}
+
+  ngOnInit(): void {}
 
 }
