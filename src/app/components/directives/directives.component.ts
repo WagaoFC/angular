068ff632246af6ad9227fbbf3156048f08a8f2a9
WagaoFC <wagnerferreira_07@hotmail.com>
2023-05-02 22:38:49 -0300

    :sparkles: feat: validando diretivas do angular

--- src/app/components/directives/directives.component.ts
+++ src/app/components/directives/directives.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
 })
-export class DirectivesComponent {
+export class DirectivesComponent implements OnInit {
+  size = 40;
+  font = 'Arial';
+  color = 'red';
 
+  classes = ['green-title', 'small-title'];
+
+  underline = 'underline-title';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
