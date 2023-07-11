2014705d540cf45dc49b3735e04b65010d8fdf7c
WagaoFC <wagnerferreira_07@hotmail.com>
2023-07-10 22:49:38 -0300

    :bug: fix: cleaning structure

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent implements OnInit{
-  name: string = 'WagaoFC'
-  age: number = 28
-  job: string = 'Developer'
 
-  constructor(){}
-
-  ngOnInit(): void {}
+export class FirstComponentComponent {
 
 }
