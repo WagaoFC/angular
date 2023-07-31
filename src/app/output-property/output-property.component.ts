18fdaf501dff27d102b36edd6ca74215192bbaa2
wagner.costa <wagner.costa@bit.com.br>
2023-07-31 11:42:48 -0300

    :sparkles: practicing Template ViewChild

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contador',
@@ -9,13 +9,15 @@ export class OutputPropertyComponent {
   @Input() valor: any = 0
   @Output() mudouValor = new EventEmitter
 
+  @ViewChild('campoInput') campoValorInput!: ElementRef
+
   incrementa() {
-    this.valor++
+    this.campoValorInput.nativeElement.value++
     this.mudouValor.emit({ novoValor: this.valor })
   }
 
   decrementa() {
-    this.valor--
+    this.campoValorInput.nativeElement.value--
     this.mudouValor.emit({ novoValor: this.valor })
   }
 }
