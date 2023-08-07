86f31166415cf7a45afe110e5fd6daacf1753d24
wagner.costa <wagner.costa@bit.com.br>
2023-08-07 14:30:35 -0300

    :sparkles: add SERVICOS

---
+++ SERVICOS/src/app/cursos/cursos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+  cursos: string[] = []
+
+  constructor(private cursosService: CursosService) {
+    this.cursosService = cursosService
+  }
+
+  ngOnInit() {
+    this.cursos = this.cursosService.getCursos()
+  }
+}
