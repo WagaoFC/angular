847dca631f17c2594e9d15c6e137011829f5cd12
wagner.costa <wagner.costa@bit.com.br>
2023-09-06 17:05:06 -0300

    :sparkles: feat: ajustando rotas cursos

--- ROTAS/src/app/app.module.ts
+++ ROTAS/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
@@ -22,8 +21,7 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    routing
+    AppRoutingModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
--- ROTAS/src/app/app.routing.module.ts
+++ ROTAS/src/app/app.routing.module.ts
@@ -1,5 +1,5 @@
 import { RouterModule, Routes } from "@angular/router";
-import { ModuleWithProviders } from "@angular/core";
+import { NgModule } from "@angular/core";
 
 import { LoginComponent } from "./login/login.component";
 import { CursosComponent } from "./cursos/cursos.component";
@@ -7,7 +7,7 @@ import { HomeComponent } from "./home/home.component";
 import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
 import { CursoNaoEncontradoComponent } from "./cursos/curso-nao-encontrado/curso-nao-encontrado.component";
 
-const APP_ROUTES: Routes = [
+const appRoutes: Routes = [
   { path: 'cursos', component: CursosComponent },
   { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
@@ -15,4 +15,9 @@ const APP_ROUTES: Routes = [
   { path: '', component: HomeComponent }
 ]
 
-export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(APP_ROUTES)
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
